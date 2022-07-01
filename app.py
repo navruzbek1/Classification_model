6fd5a31d84e17dbf71be85c9a51cad678b30cf6d
Navruzbek Abduganiyev <99237483+navruzbek1@users.noreply.github.com>
2022-07-01 23:50:17 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,71 +1,63 @@
 
 import streamlit as st
-try:
-    import numpy as np
-    import pandas as pd
-    import pickle
-    import time
-    from xgboost import XGBClassifier
-except ModuleNotFoundError as e:
-    st.error(
-        f"Looks like requirements are not installed: '{e}'. Run the following command to install requirements"
-    )
+import numpy as np
+import pandas as pd
+import pickle
+import time
+from xgboost import XGBClassifier
+st.title("Klassifikatsiya qiluvchi model..")
 
-else:
+number = st.number_input('Age')
+option = st.selectbox(
+            'Gender?',
+            ('F', 'M'))
+st.write('gender is selected:', option)
+if option:
+            if option == "M": 
+                option = int(2)
+                option = option/2
+            if option == "F":
+                option = int(1)
+                
+                
+number1 = st.number_input('Height_cm')
 
-    st.title("Klassifikatsiya qiluvchi model..")
+number2 = st.number_input('Weight_kg')
 
-    number = st.number_input('Age')
-    option = st.selectbox(
-        'Gender?',
-        ('F', 'M'))
-    st.write('gender is selected:', option)
-    if option:
-        if option == "M": 
-            option = int(2)
-            option = option/2
-        if option == "F":
-            option = int(1)
-            
-            
-    number1 = st.number_input('Height_cm')
+number3 = st.number_input('Body_fat')
 
-    number2 = st.number_input('Weight_kg')
+number4 = st.number_input('Diastolic')
 
-    number3 = st.number_input('Body_fat')
+number5 = st.number_input('Systolic')
 
-    number4 = st.number_input('Diastolic')
+number6 = st.number_input('GripForse')
 
-    number5 = st.number_input('Systolic')
+number7 = st.number_input('sit_and_bend_forward_cm')
+        
+number8 = st.number_input('sit_ups_counts')
 
-    number6 = st.number_input('GripForse')
+number9 = st.number_input('broad_jump_cm')
 
-    number7 = st.number_input('sit_and_bend_forward_cm')
-    
-    number8 = st.number_input('sit_ups_counts')
-
-    number9 = st.number_input('broad_jump_cm')
-
-    with open("xgb_model.pkl", 'rb') as file:
-        model = pickle.load(file)
-                    
-    result = st.button("Yuborish")
-    st.write(result)
-    if result:
-            st.write()
-            arr = np.array([number,option,number1,number2,number3,number4,number5,number6,number7,number8,number9])
-            if arr.all():
-            #arr = np.asanyarray([24,1,152,42,23,56,111,27,14,49,184])
-            #arr
-                    with st.spinner('Wait for it...'):
-                       time.sleep(1)
-                    st.success('Done!')
-                    pred = model.predict([arr])
-                    if pred==1:
-                            st.success("Class: A")
-                    if  pred==2:
-                            st.success("Class: B")
-                    if pred==3:
-                            st.success("Class: C")
-                    if pred==4:
-                            st.success("Class: D")
+with open("xgb_model.pkl", 'rb') as file:
+     model = pickle.load(file)
+                        
+result = st.button("Yuborish")
+st.write(result)
+if result:
+                st.write()
+                arr = np.array([number,option,number1,number2,number3,number4,number5,number6,number7,number8,number9])
+                if arr.all():
+                #arr = np.asanyarray([24,1,152,42,23,56,111,27,14,49,184])
+                #arr
+                        with st.spinner('Wait for it...'):
+                           time.sleep(1)
+                        st.success('Done!')
+                        pred = model.predict([arr])
+                        if pred==1:
+                                st.success("Class: A")
+                        if  pred==2:
+                                st.success("Class: B")
+                        if pred==3:
+                                st.success("Class: C")
+                        if pred==4:
+                                st.success("Class: D")
